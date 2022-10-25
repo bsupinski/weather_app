4229479ddbf86bd7d150f7a4fc134b0e357c85e1
Bsupinski <66806979+bsupinski@users.noreply.github.com>
2022-10-25 16:03:09 -0400

    changed fetch
    updated from http to https

--- js/model.js
+++ js/model.js
@@ -84,7 +84,7 @@ const createFiveDayForecast = (data) => {
 export const fetchWeather = async function (coords) {
   try {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=${KEY}&q=${coords}&days=5&aqi=yes&alerts=no`
+      `https://api.weatherapi.com/v1/forecast.json?key=${KEY}&q=${coords}&days=5&aqi=yes&alerts=no`
     );
     const data = await response.json();
 
